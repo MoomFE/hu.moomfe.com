1c3fb8c612b5e140f5303fcfb3be4a042e90bffe
Zhang-Wei-666 <994039348@qq.com>
2019-03-01 00:04:18 +0800

    Create config.js

---
+++ .vuepress/config.js
@@ -0,0 +1,24 @@
+
+
+module.exports = {
+  title: 'Hu.js',
+
+
+
+  themeConfig: {
+    nav: [
+      {
+        text: '学习',
+        items: [
+          { text: '教程', link: '' },
+          { text: 'API', link: '' }
+        ]
+      }
+    ]
+  },
+
+
+
+  evergreen: true,
+  dest: 'dist'
+}
